53550ee8e4eb1790ef0e24b3668c2164e1d9bfca
Vadim <Matsulmoscow@gmail.com>
2022-07-11 08:37:25 +0300

    create useInput hook

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React, { useState } from 'react';
 import './App.css';
+import { useInput } from './hooks/useInput';
 
 function App() {
 
-
-
-
+ const username = useInput ('')
+ const userpass = useInput ('')
+ console.log(username);
 
   return (
     <div className={"App"}>
-     
+     <input
+          {...username}
+          type='text' 
+          placeholder='Имя'     />
+     <input
+          {...userpass}
+          type='password' 
+          placeholder='Пароль'  />
     </div>
   )
 }
---
+++ src/hooks/useInput.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react"
+
+
+export const useInput = (initialValue) => {
+ 
+ const [ value, setValue] = useState (initialValue)
+
+ const onChange = (el) => {
+    setValue ( el.target.value )
+ }
+
+ return {
+    value,
+    onChange
+ }
+ 
+}
