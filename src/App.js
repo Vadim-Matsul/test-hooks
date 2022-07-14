41a5891ccd43f5ee069a29601253d2e990d00f44
Vadim <Matsulmoscow@gmail.com>
2022-07-14 08:54:21 +0300

    one more time created useInput

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
-import React, { useState } from 'react';
+import React  from 'react';
 import './App.css';
-import Debounce from './components/Debounce';
-import Hover from './components/Hover';
-import Inputs from './components/Inputs';
-import Request from './components/Request';
-import TodoList from './components/TodosList'
+import MyInput from './components/MyInput';
 
 
 function App() {
@@ -13,16 +9,8 @@ function App() {
 
   return (
     <div className={"App"}>
-      <h2>useInput</h2>
-      <Inputs   />
-      <h2>useHover</h2>
-      <Hover    />
-      <h2>useScroll</h2>
-      <TodoList />
-      <h2>useDebounce</h2>
-      <Debounce />
-      <h2>useRequest</h2>
-      <Request  />
+      <h2> useInput </h2>
+      <MyInput />
     </div>
   )
 }
---
+++ src/components/MyInput.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { useInput } from "../hooks/useInput";
+
+const MyInput = () => {
+
+ const greeting = useInput ()
+ const userMood = useInput ()
+
+    return (
+        <div>
+            <input 
+                { ...greeting }
+                placeholder="Hello, React"
+                />
+            <input 
+                { ...userMood }
+                placeholder="How are you?!"
+                />
+        </div>
+    )
+}
+
+
+export default MyInput;
