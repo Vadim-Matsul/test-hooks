a9681222672243484891ec19abf340d37e60df41
Vadim <Matsulmoscow@gmail.com>
2022-07-15 16:01:25 +0300

    another promise test

---
+++ src/components/BallBlock.jsx
@@ -0,0 +1,35 @@
+
+const BallBLock = () => {
+
+ const showCircle = () => {
+    addCircle( 100 ).then( div => div.append('Hello, React!'))
+ }
+
+ const addCircle = ( size ) => {
+    let   div = document.createElement('div')
+    let block = document.querySelector('.BallBlock')
+        div.style.width  = 0
+        div.style.height = 0
+        div.classList.add('Circle')
+            block.append(div)
+
+    return new Promise ( resolve => {
+        setTimeout (() => {
+            div.style.width  = size*2 + 'px'
+            div.style.height = size*2 + 'px'
+
+         div.addEventListener('transitionend', () => resolve(div) )
+        })
+    })
+ }
+
+
+    return (
+        <div className = {'BallBlock'}>
+            <button onClick = {() => showCircle()}>Нажми на меня!</button>
+        </div>
+    )
+}
+
+
+export default BallBLock;
