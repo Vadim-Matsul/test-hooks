ba67f7164bb54a9816ae8a1c3a550c013d7491a6
Vadim <Matsulmoscow@gmail.com>
2022-07-15 20:26:57 +0300

    remembered the promises

--- src/utils/promise.js
+++ src/utils/promise.js
@@ -1,34 +1,46 @@
-let promise = new Promise ( resolve => {
-   
+let promise = new Promise ( ( resolve, reject ) => {
     setTimeout(() => {
-      console.log('Идёт запрос данных на сервер...')
-      const backend = {
-        status: 200,
-        server: 'axios',
-        port: 3000
-      }
-     resolve (backend)
+        const backend = {
+            port: 3000,
+            status: 200,
+            connect: true
+        }
+        console.log('Запрос данных с сервера...')
+        resolve(backend)
     }, 1000)
 })
 
 promise.then( data => {
-    return new Promise ( resolve => {
+    return new Promise( (resolve, reject) => {
        setTimeout(() => {
-         console.log('Изменяем объект', data)
-         data.modified = true
-        resolve(data)
-       },1000)
+        console.log('Изменяем объект:', data)
+          data.protocol = 'http'
+          resolve(data)
+       },2000)
     })
- })
- .then( newData => {
-    return new Promise (resolve => {
+ }
+).then( newData => {
+  
+        console.log('__________ _________ __________')
+    
+        console.log('Обьект изменён', newData)
+ 
+})
+.catch( error => console.error('Ошибка:', error))
+.finally( () => console.log('Запрос завершён'))
+
+const delay = ms => {
+    return new Promise ( resolve => {
         setTimeout(() => {
-            console.log('повторно изменяем', newData)
-            newData.fromPromise = true
-            resolve( newData )
-        },2000) 
+            resolve()
+        }, ms)
     })
- })
- .then( data => { console.log('Modified', data)})
+}
+// delay(2000).then(() => console.log('After 2 seconds'))
+// delay(3000).then(() => console.log('After 3 seconds'))
 
+Promise.all( [delay(1000), delay(5000)] )
+.then( () => console.log('Все промисы выполнены'))
 
+Promise.race([delay(2000), delay(3000)])
+.then( () => console.log('Выполнен первый промис') )
